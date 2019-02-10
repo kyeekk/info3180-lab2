e517ffab2c2641183f2ec8f765c8ec02c2f07f78
Kayla Effs <kaylaeffs@gmail.com>
2019-02-10 00:27:11 +0000

    date joined function

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -12,13 +12,6 @@ from flask import render_template, request, redirect, url_for, flash
 ###
 # Routing for your application.
 ###
-
-@app.route('/profile')
-def profilr():
-    """profile"""
-    return render_template('profile.html')
-
-
 
 @app.route('/')
 def home():
@@ -31,6 +24,11 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+
+@app.route('/profile/')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html', date_joined=format_date_joined(2019, 2, 6))
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -60,6 +58,14 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
+
+def format_date_joined(year, month, day):
+    """
+    Takes a date as an argument and return a string in the format: MMM, YYYY
+    eg. Feb, 2019
+    """
+    date_joined = datetime.date(year, month, day)
+    return "Joined " + date_joined.strftime("%B, %Y")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
